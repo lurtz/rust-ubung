4e55f40362b85b0fb0679a44d6c4350f66da7899
Lutz <derlurtz@gmail.com>
2016-10-23 10:58:06 +0200

    update rust, vim-rust and used libraries

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -5,8 +5,8 @@ extern crate piston_window;
 extern crate image;
 extern crate num;
 
-use conrod::{Canvas, Theme, Widget};
-use conrod::{Image, Positionable, Sizeable};
+use conrod::{Widget, widget};
+use conrod::{Positionable, Sizeable};
 use piston_window::{EventLoop, OpenGL, PistonWindow, UpdateEvent, WindowSettings};
 use piston_window::{ImageSize, G2dTexture, Texture, Size};
 use image::{ImageBuffer, Pixel};
@@ -28,16 +28,19 @@ fn main() {
     window.set_ups(60);
 
     // construct our `Ui`.
-    let mut ui = conrod::Ui::new(Theme::default());
+    let mut ui = conrod::UiBuilder::new().build();
 
     // Create a texture to use for efficiently caching text on the GPU.
     let mut text_texture_cache =
         conrod::backend::piston_window::GlyphCache::new(&mut window, WIDTH, HEIGHT);
 
+    // Instantiate the generated list of widget identifiers.
+    let ids = &mut Ids::new(ui.widget_id_generator());
+
     // The image map describing each of our widget->image mappings (in our case, none).
     //let image_map = conrod::image::Map::new();
     let image_map = image_map! {
-        (RUST_LOGO, rust_logo(&mut window)),
+        (ids.rust_logo, rust_logo(&mut window)),
     };
 
 
@@ -49,21 +52,24 @@ fn main() {
             ui.handle_event(e);
         }
 
-        event.update(|_| ui.set_widgets(|mut ui| {
-          // Construct our main `Canvas` tree.
-          Canvas::new().set(MASTER, &mut ui);
 
-          let (w, h) = image_map.get(RUST_LOGO).unwrap().get_size();
+        event.update(|_| {
+          let ref mut uicell = ui.set_widgets();
+          // Construct our main `Canvas` tree.
+          widget::Canvas::new().set(ids.master, uicell);
+
+          let (w, h) = image_map.get(&ids.rust_logo).unwrap().get_size();
 
           // Instantiate the `Image` at its full size in the middle of the window.
-          Image::new().w_h(w as f64, h as f64).middle().set(RUST_LOGO, &mut ui);
-        }));
+          widget::Image::new().w_h(w as f64, h as f64).middle().set(ids.rust_logo, uicell);
+        });
 
         window.draw_2d(&event, |c, g| {
-            if let Some(primitives) = ui.draw_if_changed(&image_map) {
+            if let Some(primitives) = ui.draw_if_changed() {
                 fn texture_from_image<T>(img: &T) -> &T { img };
                 conrod::backend::piston_window::draw(c, g, primitives,
                                                      &mut text_texture_cache,
+                                                     &image_map,
                                                      texture_from_image);
             }
         });
@@ -85,26 +91,86 @@ fn rust_logo(window: &mut PistonWindow) -> G2dTexture {
 
 fn px_func(x: u32, y: u32, size: Size) -> image::Rgba<u8> {
     let iterations = 1000;
-    let mut i: u8 = 0;
+    let mut i: u32 = 0;
     let cx = (x as f32 - size.width as f32/2.0) / (size.width as f32 / 4.0);
     let cy = (y as f32 - size.height as f32/2.0) / (size.height as f32 / 4.0);
     let c = Complex::new(cx, cy);
     let mut z = c;
-    while i <= iterations && z.norm() < 1000.0 {
+    while i < iterations && z.norm() < 1000.0 {
         z = z*z + c;
         i = i+1;
     }
-    let transparency = (iterations+1-i) * (u8::max_value()/(iterations+1));
-    image::Rgba::<u8>::from_channels(127, 127, 127, transparency)
+    let transparency: f32 = (iterations-i) as f32 * ((u8::max_value() as f32)/(iterations as f32));
+    let transformed_transparency = transform_transparency(transparency);
+    if y < 400 {
+        println!("x: {}, y: {}, i: {}, transparency: {}, transparency as u8: {}, transformed_transparency: {}", x, y, i, transparency, transparency as u8, transformed_transparency);
+    }
+    image::Rgba::<u8>::from_channels(127, 127, 127, transformed_transparency)
+}
+
+/*
+fn taken_iterations_to_rgba(i: u32, max_iterations: u32) -> (u8, u8, u8, u8) {
+    // i > max_iterations                                -> (  0,   0, 0, 255)
+    // i < max_iterations && i >= max_iterations*2/3     -> (  r,   0, 0, 255)
+    // i < max_iterations*2/3 && i >= max_iterations*1/3 -> (255,   g, 0, 255)
+    // i < max_iterations*1/3                            -> (255, 255, b, 255)
+    let red_border = max_iterations * 2 / 3;
+    let green_border = max_iterations * 1 / 3;
+
+    let mut r = 0;
+    if i < max_iterations && i > red_border {
+        let upper_bound = max_iterations - red_border;
+        let reduced_i = i - red_border;
+        r = ((reduced_i as f32 / upper_bound as f32) * 254.0) as u8;
+        r = 255 - r;
+    }
+    if i <= red_border {
+        i = 255;
+    }
+
+    let mut g = 0;
+    if i < red_border && i >= green_border {
+        let upper_bound = red_border - green_border;
+        let reduced_i = i - green_border;
+        g = ((reduced_i as f32 / upper_bound as f32) * 254.0) as u8;
+        g = 255 - g;
+    }
+    if i <= green_border {
+        g = 255;
+    }
+
+    let mut b = 0;
+    if i < green_border {
+    }
+
+    let mut a = 255;
+    (r,g,b,a)
+}
+*/
+
+fn transform_transparency(transparency: f32) -> u8 {
+    //   0 .. 199 -> 0
+    // 200 .. 255 -> 1 .. 255
+
+    let border = 240.0;
+    if transparency < border {
+        return 0
+    }
+
+    let upper_bound = u8::max_value() as f32 - border;
+    let reduced_transparency = transparency - border;
+    ((reduced_transparency / upper_bound) * 254.0 + 1.0) as u8
 }
 
 // Generate a unique `WidgetId` for each widget.
 widget_ids! {
 
+    struct Ids {
     // Canvas IDs.
-    MASTER,
+    master,
 
     // image
-    RUST_LOGO
+    rust_logo,
+    }
 }
 
