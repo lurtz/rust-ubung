2c4d77a16d81a58dfc03fc5f8bcfa8334fca0bb6
Lutz <derlurtz@gmail.com>
2016-12-12 10:56:53 +0100

    only one loop in test

--- romans/src/main.rs
+++ romans/src/main.rs
@@ -22,10 +22,10 @@ static DIST_TO_STRING : [(u32, &'static str); 4] = [
 
 #[test]
 fn positive_nonzero_numbers() {
-    for item in VALUE_TO_STRING.iter() {
-        assert_ne!(0, item.0);
-    }
-    for item in DIST_TO_STRING.iter() {
+    use std::iter::FromIterator;
+    let mut data = Vec::from_iter(VALUE_TO_STRING.iter());
+    data.extend(DIST_TO_STRING.iter());
+    for item in data {
         assert_ne!(0, item.0);
     }
 }
