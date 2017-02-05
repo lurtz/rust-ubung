84a0232d54bc06f3f9c580987e5e1ba048258c09
Lutz <derlurtz@gmail.com>
2017-02-05 17:06:58 +0100

    add implmentation which communicates with the denon avr in a seperate thread

---
+++ denon-control/src/denon_connection.rs
@@ -0,0 +1,242 @@
+use std::collections::HashMap;
+use std::time::Duration;
+use std::net::TcpStream;
+use std::sync::mpsc::{channel, Sender, Receiver};
+use std::sync::{Arc, Mutex};
+use std::thread::JoinHandle;
+use std::thread;
+use std::error::Error;
+use std::io::{Read, Write};
+use std::fmt::{Display, Formatter};
+
+extern crate std;
+
+#[derive(Hash, Eq, PartialEq, Debug, Clone)]
+pub enum Operation {
+    MaxVolume,
+    MainVolume,
+    Power,
+    SourceInput,
+    Stop,
+}
+
+impl Operation {
+    fn value(&self) -> &'static str {
+        match *self {
+            Operation::MaxVolume => "MVMAX",
+            Operation::MainVolume => "MV",
+            Operation::Power => "PW",
+            Operation::SourceInput => "SI",
+            Operation::Stop => "really stop now",
+        }
+    }
+
+    fn parse(str: &str) -> Option<(Operation, State)> {
+        let trimmed = str.trim().trim_matches('\r');
+        if trimmed.starts_with(Operation::MaxVolume.value()) {
+            let to_skip = Operation::MaxVolume.value().len();
+            let ref to_parse = trimmed[to_skip..].trim();
+            let mut value = to_parse.parse::<u32>().unwrap();
+            if value < 100 {
+                value = value * 10;
+            }
+            return Some((Operation::MaxVolume, State::Integer(value)));
+        }
+        if trimmed.starts_with(Operation::MainVolume.value()) {
+            let to_skip = Operation::MainVolume.value().len();
+            let ref to_parse = trimmed[to_skip..].trim();
+            let mut value = to_parse.parse::<u32>().unwrap();
+            if value < 100 {
+                value = value * 10;
+            }
+            return Some((Operation::MainVolume, State::Integer(value)));
+        }
+        if trimmed.starts_with(Operation::Power.value()) {
+            let to_skip = Operation::Power.value().len();
+            let value = trimmed[to_skip..].to_string();
+            return Some((Operation::Power, State::String(value)));
+        }
+        if trimmed.starts_with(Operation::SourceInput.value()) {
+            let to_skip = Operation::SourceInput.value().len();
+            let value = trimmed[to_skip..].to_string();
+            return Some((Operation::SourceInput, State::String(value)));
+        }
+        None
+    }
+}
+
+impl Display for Operation {
+    fn fmt(&self, format: &mut Formatter) -> Result<(), std::fmt::Error> {
+        write!(format, "{}", self.value())
+    }
+}
+
+#[derive(Debug,Clone)]
+pub enum State {
+    Integer(u32),
+    String(String),
+}
+
+impl Display for State {
+    fn fmt(&self, format: &mut Formatter) -> Result<(), std::fmt::Error> {
+        match self {
+            &State::Integer(i) => write!(format, "{}", i),
+            &State::String(ref s) => write!(format, "{}", s),
+        }
+    }
+}
+
+pub fn write(stream: &mut Write, input: String) -> Result<(), std::io::Error> {
+    println!("sending: {}", input);
+    let volume_command = input.into_bytes();
+    stream.write(&volume_command[..])?;
+    Ok(())
+}
+
+pub fn read(stream: &mut Read, lines: u8) -> Result<Vec<String>, std::io::Error> {
+    let mut string = String::new();
+
+    for _ in 0..lines {
+        let mut buffer = [0; 100];
+        let read_bytes = stream.read(&mut buffer)?;
+
+        if let Ok(tmp) = std::str::from_utf8(&buffer[0..read_bytes]) {
+            string += tmp;
+        }
+    }
+
+    string.pop();
+
+    let string_iter = string.split('\r').map(|x| String::from(x));
+    let result = string_iter.collect();
+    println!("{:?}", result);
+    Ok(result)
+}
+
+fn thread_func_impl(denon_name: String,
+                    denon_port: u16,
+                    state: Arc<Mutex<HashMap<Operation, State>>>,
+                    requests: Receiver<(Operation, State)>)
+                    -> Result<(), std::io::Error> {
+    let mut stream = TcpStream::connect((denon_name.as_str(), denon_port))?;
+    stream.set_read_timeout(Some(Duration::from_secs(1)))?;
+
+    loop {
+        if let Ok((request, value)) = requests.try_recv() {
+            if Operation::Stop == request {
+                return Ok(());
+            }
+            let command = format!("{}{}\r", request, value);
+            write(&mut stream, command)?;
+        }
+
+        match read(&mut stream, 1) {
+            Ok(status_update) => {
+                println!("received update {:?}", status_update);
+                let parsed_response = parse_response(&status_update);
+                let mut locked_state = state.lock().unwrap();
+                for item in parsed_response {
+                    locked_state.insert(item.0, item.1);
+                }
+            }
+            // check for timeout error -> continue on timeout error, else abort
+            Err(e) => {
+                if std::io::ErrorKind::TimedOut != e.kind() &&
+                   std::io::ErrorKind::WouldBlock != e.kind() {
+                    return Err(e);
+                }
+            }
+        }
+    }
+}
+
+fn parse_response(response: &Vec<String>) -> Vec<(Operation, State)> {
+    let mut result = Vec::new();
+    for item in response {
+        if let Some(parsed) = Operation::parse(item.as_str()) {
+            result.push(parsed);
+        }
+
+    }
+    result
+}
+
+pub fn print_io_error(e: &std::io::Error) {
+    println!("got error: {}, cause = {:?}, description = {}, kind = {:?}",
+             e,
+             e.cause(),
+             e.description(),
+             e.kind());
+    if let Some(raw_os_error) = e.raw_os_error() {
+        println!("raw_os_error = {}", raw_os_error);
+    }
+    if let Some(inner) = e.get_ref() {
+        println!("inner = {}", inner);
+    }
+}
+
+fn thread_func(denon_name: String,
+               denon_port: u16,
+               state: Arc<Mutex<HashMap<Operation, State>>>,
+               requests: Receiver<(Operation, State)>) {
+    match thread_func_impl(denon_name, denon_port, state, requests) {
+        Ok(_) => println!("thread success"),
+        Err(e) => print_io_error(&e),
+    }
+}
+
+pub struct DenonConnection {
+    state: Arc<Mutex<HashMap<Operation, State>>>,
+    requests: Sender<(Operation, State)>,
+    current_thread: JoinHandle<()>,
+}
+
+impl DenonConnection {
+    pub fn new(denon_name: &str, denon_port: u16) -> DenonConnection {
+        let denon_string = String::from(denon_name);
+        let state = Arc::new(Mutex::new(HashMap::new()));
+        let cloned_state = state.clone();
+        let (tx, rx) = channel();
+        let join_handle: JoinHandle<()> = thread::spawn(move || {
+            thread_func(denon_string, denon_port, cloned_state, rx);
+        });
+        let dc = DenonConnection {
+            state: state,
+            requests: tx,
+            current_thread: join_handle,
+        };
+        dc
+    }
+
+    pub fn get(&self, op: Operation) -> State {
+        // TODO heavily broken
+        // should first check if the requested op is present in state
+        // if it is not present it should send the request to the thread and wait until completion
+        {
+            let locked_state = self.state.lock().unwrap();
+            if let Some(state) = locked_state.get(&op) {
+                return state.clone();
+            }
+        }
+        self.set(op.clone(), State::String(String::from("?")));
+        for _ in 0..10 {
+            thread::sleep(Duration::from_millis(100));
+            let locked_state = self.state.lock().unwrap();
+            if let Some(state) = locked_state.get(&op) {
+                return state.clone();
+            }
+        }
+        State::Integer(0)
+    }
+
+    pub fn set(&self, op: Operation, state: State) {
+        self.requests.send((op.clone(), state)).unwrap();
+    }
+}
+
+impl Drop for DenonConnection {
+    fn drop(&mut self) {
+        self.set(Operation::Stop, State::Integer(0));
+        // self.current_thread.join();
+    }
+}
--- denon-control/src/main.rs
+++ denon-control/src/main.rs
@@ -6,16 +6,42 @@ use std::net::TcpStream;
 use std::io::{Read, Write};
 use std::time::Duration;
 use std::thread;
-use std::error::Error;
 use std::ops::Index;
 use std::fmt::Display;
 use std::str::FromStr;
+
+mod denon_connection;
+
+use denon_connection::{DenonConnection, Operation, print_io_error, read, write};
 
 #[cfg(test)]
 mod test {
     #[test]
     fn bla() {
         assert_eq!(2, 2);
+    }
+}
+
+// status object shall get the current status of the avr 1912
+// easiest way would be a map<Key, Value> where Value is an enum of u32 and String
+// Key is derived of a mapping from the protocol strings to constants -> define each string once
+// the status object can be shared or the communication thread can be asked about a
+// status which queries the receiver if it is not set
+
+#[derive(Hash, Eq, PartialEq, Debug)]
+enum Operations {
+    MainVolume,
+    Power,
+    SourceInput,
+}
+
+impl Operations {
+    fn value(&self) -> &'static str {
+        match *self {
+            Operations::MainVolume => "MV",
+            Operations::Power => "PW",
+            Operations::SourceInput => "SI",
+        }
     }
 }
 
@@ -46,41 +72,18 @@ fn do_it(denon_name: &str, denon_port: u16) -> Result<(), std::io::Error> {
     Ok(())
 }
 
-fn write(stream: &mut Write, input: String) -> Result<(), std::io::Error> {
-    println!("sending: {}", input);
-    let volume_command = input.into_bytes();
-    stream.write(&volume_command[..])?;
-    Ok(())
-}
 
-fn read(stream: &mut Read, lines: u8) -> Result<Vec<String>, std::io::Error> {
-    let mut string = String::new();
-
-    for _ in 0..lines {
-        let mut buffer = [0; 100];
-        let read_bytes = stream.read(&mut buffer)?;
-
-        if let Ok(tmp) = std::str::from_utf8(&buffer[0..read_bytes]) {
-            string += tmp;
-        }
-    }
-
-    string.pop();
-
-    let string_iter = string.split('\r').map(|x| String::from(x));
-    let result = string_iter.collect();
-    println!("{:?}", result);
-    Ok(result)
-}
-
+// TODO add expected output
+// TODO parse responses from denon asyncronously because they must not really
+//      have to do to something with the command
 pub struct ControlElement<T> {
-    prefix: &'static str,
+    prefix: Operations,
     num_responses: u8,
     result_parser: Box<Fn(&str) -> T>,
 }
 
 impl<T> ControlElement<T> {
-    fn new<F>(prefix: &'static str, num_responses: u8, result_parser: F) -> ControlElement<T>
+    fn new<F>(prefix: Operations, num_responses: u8, result_parser: F) -> ControlElement<T>
         where F: Fn(&str) -> T + 'static
     {
         ControlElement {
@@ -91,17 +94,18 @@ impl<T> ControlElement<T> {
     }
 }
 
-
 pub fn volume() -> ControlElement<u32> {
-    ControlElement::new("MV", 2, |x| u32::from_str(x).unwrap())
+    ControlElement::new(Operations::MainVolume, 2, |x| u32::from_str(x).unwrap())
 }
 
 pub fn power() -> ControlElement<bool> {
-    ControlElement::new("PW", 1, |x| if "ON" == x { true } else { false })
+    ControlElement::new(Operations::Power,
+                        1,
+                        |x| if "ON" == x { true } else { false })
 }
 
 pub fn input() -> ControlElement<String> {
-    ControlElement::new("SI", 2, |x| String::from(x))
+    ControlElement::new(Operations::SourceInput, 2, |x| String::from(x))
 }
 
 fn process<T, X>(stream: &mut T,
@@ -110,14 +114,15 @@ fn process<T, X>(stream: &mut T,
                  -> Result<X, std::io::Error>
     where T: Write + Read
 {
-    let volume_command_string = format!("{}{}\r", ce.prefix, value);
+    let prefix = ce.prefix.value();
+    let volume_command_string = format!("{}{}\r", prefix, value);
     write(stream, volume_command_string)?;
 
     let result = read(stream, ce.num_responses)?;
 
     for volume_string in result {
-        if volume_string.starts_with(ce.prefix) {
-            let actual_value = volume_string.index(ce.prefix.len()..volume_string.len());
+        if volume_string.starts_with(prefix) {
+            let actual_value = volume_string.index(prefix.len()..volume_string.len());
             let result = (ce.result_parser)(actual_value);
             return Ok(result);
         }
@@ -126,28 +131,19 @@ fn process<T, X>(stream: &mut T,
     Err(std::io::Error::from(std::io::ErrorKind::InvalidData))
 }
 
-fn print_io_error(e: &std::io::Error) {
-    println!("got error: {}, cause = {:?}, description = {}, kind = {:?}",
-             e,
-             e.cause(),
-             e.description(),
-             e.kind());
-    if let Some(raw_os_error) = e.raw_os_error() {
-        println!("raw_os_error = {}", raw_os_error);
-    }
-    if let Some(inner) = e.get_ref() {
-        println!("inner = {}", inner);
-    }
-}
-
 fn main() {
     let denon_name = "0005cd221b08.lan";
     let denon_port = 23;
 
-    match do_it(denon_name, denon_port) {
-        Ok(_) => println!("success"),
-        Err(e) => {
-            print_io_error(&e);
-        }
-    }
+    //    match do_it(denon_name, denon_port) {
+    //        Ok(_) => println!("success"),
+    //        Err(e) => {
+    //            print_io_error(&e);
+    //        }
+    //    }
+
+    let dc = DenonConnection::new(denon_name, denon_port);
+    println!("{:?}", dc.get(Operation::Power));
+    println!("{:?}", dc.get(Operation::MainVolume));
+    thread::sleep(Duration::from_secs(30));
 }
