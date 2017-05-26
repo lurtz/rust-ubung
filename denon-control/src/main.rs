6ea4fa07c57b9ce855dafc19437fda00ab8fea13
Lutz <derlurtz@gmail.com>
2017-05-26 18:11:26 +0200

    no GUI, it is just a pain in the ass (at the moment)

--- denon-control/src/main.rs
+++ denon-control/src/main.rs
@@ -2,13 +2,6 @@
 // MV53
 // MVMAX 86
 
-#[macro_use]
-extern crate conrod;
-// extern crate piston_window;
-// extern crate image;
-// extern crate num;
-// extern crate gfx_core;
-
 mod denon_connection;
 mod state;
 mod operation;
@@ -19,17 +12,6 @@ use std::thread;
 
 use denon_connection::{DenonConnection, State};
 use state::PowerState;
-
-use conrod::{Widget, UiCell, Positionable, Sizeable, UiBuilder};
-use conrod::widget::{Canvas, Image};
-use conrod::image::Map;
-use conrod::backend::piston_window::{GlyphCache, convert_event, draw};
-// use piston_window::{EventLoop, PistonWindow, UpdateEvent, WindowSettings};
-// use piston_window::{ImageSize, G2dTexture, Texture, Size, Window, OpenGL};
-// use piston_window::{Event, TextureSettings};
-// use image::{ImageBuffer, Pixel};
-// use num::complex::Complex;
-// use gfx_core::Resources;
 
 #[cfg(test)]
 mod test {
@@ -72,76 +54,3 @@ fn main() {
     thread::sleep(Duration::from_secs(5));
 }
 
-// =======================================================
-
-fn main2() {
-    const WIDTH: u32 = 800;
-    const HEIGHT: u32 = 600;
-
-    // Change this to OpenGL::V2_1 if not working.
-    let opengl = OpenGL::V3_3;
-
-    // Construct the window.
-    let mut window: PistonWindow = WindowSettings::new("Canvas Demo", [WIDTH, HEIGHT])
-        .opengl(opengl)
-        .exit_on_esc(true)
-        .vsync(true)
-        .build()
-        .unwrap();
-    window.set_ups(60);
-
-    // construct our `Ui`.
-    let mut ui = UiBuilder::new().build();
-
-    // Create a texture to use for efficiently caching text on the GPU.
-    let mut text_texture_cache = GlyphCache::new(&mut window, WIDTH, HEIGHT);
-
-    // Instantiate the generated list of widget identifiers.
-    let ids = &mut Ids::new(ui.widget_id_generator());
-
-    // Poll events from the window.
-    while let Some(event) = window.next() {
-
-        // Convert the piston event to a conrod event.
-        if let Some(e) = convert_event(event.clone(), &window) {
-            ui.handle_event(e);
-        }
-
-        event.update(|_| {
-            let uicell = ui.set_widgets();
-            set_widgets(uicell, ids);
-        });
-
-        window.draw_2d(&event, |c, g| {
-            if let Some(primitives) = ui.draw_if_changed() {
-                fn texture_from_image<T>(img: &T) -> &T {
-                    img
-                };
-                draw(c,
-                     g,
-                     primitives,
-                     &mut text_texture_cache,
-                     &image_map,
-                     texture_from_image);
-            }
-        });
-    }
-}
-
-fn set_widgets(ref mut uicell: UiCell, ids: &mut Ids) {
-    // Construct our main `Canvas` tree.
-    Canvas::new().set(ids.master, uicell);
-}
-
-// Generate a unique `WidgetId` for each widget.
-widget_ids! {
-    struct Ids {
-    // Canvas IDs.
-    master,
-
-    // image
-    rust_logo,
-
-    image_mouse,
-    }
-}
