e244f73a5429bcd21d64cd399e7b31cb222d255b
Lutz <derlurtz@gmail.com>
2017-09-29 12:23:38 +0200

    use real hostname produced by avahi-browse and not something which happens to be a hostname

--- denon-control/src/avahi.rs
+++ denon-control/src/avahi.rs
@@ -4,6 +4,7 @@ pub fn get_receiver() -> String {
     let output = Command::new("/usr/bin/avahi-browse")
         .arg("-p")
         .arg("-t")
+        .arg("-r")
         .arg("_raop._tcp")
         .output()
         .expect("avahi-browse failed");
@@ -11,15 +12,14 @@ pub fn get_receiver() -> String {
     let output_stdout = String::from_utf8_lossy(&output.stdout);
     let lines = output_stdout.lines();
     let denon_names : Vec<&str> = lines
+        .filter(|&line| line.starts_with("="))
         .filter(|&line| line.contains("DENON"))
         .map(|line| line.split(';'))
-        .map(|iter| iter.skip(3).next().unwrap())
-        .map(|stri| stri.split("\\064"))
-        .map(|mut iter| iter.next().unwrap())
+        .map(|iter| iter.skip(6).next().unwrap())
         .collect();
 
     if denon_names.len() > 1 {
-        println!("multiple receivers found, taking: {}", denon_names[0]);
+        println!("multiple receivers found: {:?}, taking: {}", denon_names, denon_names[0]);
         println!("use -a option if you want to use another receiver");
     }
 
