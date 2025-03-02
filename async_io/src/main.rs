e33328e94f06463c4c9aa864c8540f0b6ae9803a
Lutz <727209+lurtz@users.noreply.github.com>
2025-03-03 00:39:57 +0100

    a bit more complex state machine / socket interaction

--- async_io/src/main.rs
+++ async_io/src/main.rs
@@ -1,9 +1,40 @@
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::net::TcpListener;
+use std::io::ErrorKind;
 
-#[tokio::main]
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
+
+async fn read_int(socket: &mut TcpStream, mut buf: &mut [u8]) -> Result<usize, std::io::Error> {
+    let n = socket.read(&mut buf).await?;
+    if 0 == n {
+        return Err(std::io::Error::from(ErrorKind::ConnectionAborted));
+    }
+    let x: usize = str::from_utf8(&buf[0..n]).unwrap().trim().parse().unwrap();
+    Ok(x)
+}
+
+async fn read_x_and_y_and_reply_with_sum(
+    mut socket: &mut TcpStream,
+    mut buf: &mut [u8],
+) -> Result<(), std::io::Error> {
+    socket.write_all("< x = ".as_bytes()).await?;
+    let x = read_int(&mut socket, &mut buf).await?;
+    socket.write_all("< y = ".as_bytes()).await?;
+    let y = read_int(&mut socket, &mut buf).await?;
+
+    let z = x + y;
+
+    // Write the data back
+    socket
+        .write_all(format!("> z = {}\n", z).as_bytes())
+        .await?;
+
+    Ok(())
+}
+
+#[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let listener = TcpListener::bind("127.0.0.1:8080").await?;
+    println!("listening on {}", listener.local_addr()?);
 
     loop {
         let (mut socket, _) = listener.accept().await?;
@@ -13,19 +44,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
             // In a loop, read data from the socket and write the data back.
             loop {
-                let n = match socket.read(&mut buf).await {
-                    // socket closed
-                    Ok(0) => return,
-                    Ok(n) => n,
-                    Err(e) => {
-                        eprintln!("failed to read from socket; err = {:?}", e);
-                        return;
-                    }
-                };
-
-                // Write the data back
-                if let Err(e) = socket.write_all(&buf[0..n]).await {
-                    eprintln!("failed to write to socket; err = {:?}", e);
+                if let Err(e) = read_x_and_y_and_reply_with_sum(&mut socket, &mut buf).await {
+                    eprintln!("socket failure; err = {:?}", e);
                     return;
                 }
             }
